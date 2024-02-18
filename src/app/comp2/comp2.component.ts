d39ad4b4012e7082b8acfde7cd4711efdfcea412
Vlaimir Korba <krissky1986@gmail.com>
2024-02-18 22:42:55 +0200

    #59 What is View Encapsulation | Understanding View Encapsulation |

---
+++ src/app/comp2/comp2.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-comp2',
+  standalone: true,
+  imports: [],
+  templateUrl: './comp2.component.html',
+  styleUrl: './comp2.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class Comp2Component {}
